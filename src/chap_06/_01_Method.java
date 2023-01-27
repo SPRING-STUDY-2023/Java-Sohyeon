ecfa54d93269c853c8632b3473e0794bfc333453
thguss <thgus345@naver.com>
2023-01-27 14:28:34 +0900

    [DAY3] 메소드

---
+++ src/chap_06/_01_Method.java
@@ -0,0 +1,12 @@
+package chap_06; // 메소드
+
+public class _01_Method {
+
+    public static void sayHello() { // 메소드 정의
+        System.out.println("Hello Method!");
+    }
+
+    public static void main(String[] args) {
+        sayHello(); // 메소드 호출
+    }
+}
