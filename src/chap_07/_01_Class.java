1cf91833ed1af5490c2f67c214cea3cfaedfba92
thguss <thgus345@naver.com>
2023-01-29 09:00:06 +0900

    [DAY4] 클래스 변수

---
+++ src/chap_07/_01_Class.java
@@ -0,0 +1,9 @@
+package chap_07;
+
+// 객체 지향 프로그래밍 (OOP: Object Oriented Programming)
+public class _01_Class {
+    public static void main(String[] args) {
+        // 객체 생성
+        BlackBox blackBox = new BlackBox();
+    }
+}
