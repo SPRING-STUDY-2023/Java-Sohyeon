df952f6e32d539072cfb8474aeb6558ac51e3077
thguss <thgus345@naver.com>
2023-01-29 09:10:23 +0900

    [DAY4] FEAT: 클래스 생성

---
+++ src/chap_07/BlackBox.java
@@ -0,0 +1,9 @@
+package chap_07;
+
+public class BlackBox {
+    String modelName;
+    String resolution;
+    int price;
+    String color;
+    static boolean canAutoReport = false; // 자동 신고 기능
+}
